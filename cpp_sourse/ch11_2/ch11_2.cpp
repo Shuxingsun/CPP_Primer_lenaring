5146dcfcd45e0c8bc74add6dbbf0f38691d03f2e
Shuxing290 <2995298638@qq.com>
2022-04-01 22:29:38 +0800

    add ch10-12.5

---
+++ cpp_sourse/ch11_2/ch11_2.cpp
@@ -0,0 +1,30 @@
+﻿// ch11_2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <map>
+#include <set>
+#include <string>
+#include <iostream>
+
+using std::string;
+
+int main()
+{
+    std::multimap<string, string> authors{
+        { "alan", "DMA" },
+        { "pezy", "LeetCode" },
+        { "alan", "CLRS" },
+        { "wang", "FTP" },
+        { "pezy", "CP5" },
+        { "wang", "CPP-Concurrency" }
+    };
+    std::map<string, std::multiset<string>> order_authors;
+    for (const auto& author : authors)
+        order_authors[author.first].insert(author.second);
+    for (const auto& author : order_authors) {
+        std::cout << author.first << ": ";
+        for (const auto& work : author.second)
+            std::cout << work << " ";
+        std::cout << std::endl;
+    }
+}
